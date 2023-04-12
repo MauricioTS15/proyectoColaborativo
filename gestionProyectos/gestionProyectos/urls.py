76544e931100df4c0e1cd3a01b1d60f96b9b3bc5
MauricioTS15 <mauriciodavid.tavara@opendeusto.es>
2023-04-12 17:39:30 +0200

    Correcion de errores

--- gestionProyectos/gestionProyectos/urls.py
+++ gestionProyectos/gestionProyectos/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gestionProyectos/', include('gestionProyectos.urls')),
+    path('appProyectos/', include('appProyectos.urls')),
 ]
