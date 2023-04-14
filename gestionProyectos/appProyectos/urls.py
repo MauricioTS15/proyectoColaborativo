4443880ca4507a1681069b10cbf465ab7eec2b53
MauricioTS15 <mauriciodavid.tavara@opendeusto.es>
2023-04-14 16:23:36 +0200

    Creacion de sistema de visualizacion de clientes.

--- gestionProyectos/appProyectos/urls.py
+++ gestionProyectos/appProyectos/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
  path('', views.index, name='index'),
  path('proyectos/', views.index_proyectos, name='index_proyectos'),
+ path('clientes/', views.index_clientes, name='index_clientes'),
  path('proyectos/<int:proyecto_id>', views.show_proyecto, name='proyecto'),
  path('tareas/<int:tarea_id>', views.show_tarea, name='tarea'),
+ path('clientes/<int:cliente_id>', views.show_cliente, name='cliente'),
 ]
