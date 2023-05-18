6ccd47b284bfca809b059bf3d1dae2206a8c7218
MauricioTS15 <mauriciodavid.tavara@opendeusto.es>
2023-05-18 18:42:29 +0200

    Cambios JavaScript y CSS
    Co-authored-by: Rubén <Ruben-Bellido@users.noreply.github.com>

--- gestionProyectos/appProyectos/static/js/general-script.js
+++ gestionProyectos/appProyectos/static/js/general-script.js
@@ -2,20 +2,22 @@
 var aumentarBtn = document.getElementById('aumentar');
 var reducirBtn = document.getElementById('reducir');
 var restaurarBtn = document.getElementById('restaurar');
-var elementos = document.getElementsByClassName('cambiar-tamaño');
+var elementos = document.querySelectorAll('div > span');
 var tamañosOriginales = [];
 
 aumentarBtn.addEventListener('click', function () {
     for (var i = 0; i < elementos.length; i++) {
         var fontSize = parseFloat(window.getComputedStyle(elementos[i]).fontSize);
-        elementos[i].style.fontSize = (fontSize + 5) + 'px';
+        if (fontSize < 25)
+            elementos[i].style.fontSize = (fontSize + 3) + 'px';
     }
 });
 
 reducirBtn.addEventListener('click', function () {
     for (var i = 0; i < elementos.length; i++) {
         var fontSize = parseFloat(window.getComputedStyle(elementos[i]).fontSize);
-        elementos[i].style.fontSize = (fontSize - 5) + 'px';
+        if (fontSize > 15)
+            elementos[i].style.fontSize = (fontSize - 3) + 'px';
     }
 });
 
@@ -29,5 +31,17 @@ restaurarBtn.addEventListener('click', function () {
 document.addEventListener('DOMContentLoaded', function () {
     for (var i = 0; i < elementos.length; i++) {
         tamañosOriginales.push(window.getComputedStyle(elementos[i]).fontSize);
+    }
+});
+
+
+// TOGGLE DARK MODE
+let toggler = document.getElementById('toggle-darkmode');
+toggler.addEventListener('change', function toggleDarkMode() {
+    var element = document.body;
+    if (this.checked) {
+        element.classList.toggle("darkmode");
+    } else {
+        element.classList.toggle("darkmode");
     }
 });
